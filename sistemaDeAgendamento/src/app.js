d41bddac4586b8dcd078cb54b0ff313319b15398
Amauri Rodrigues dos Santos Junior <amauriaureo@gmail.com>
2021-08-25 00:36:58 -0300

    configurando sucrase e nodemon

--- sistemaDeAgendamento/src/app.js
+++ sistemaDeAgendamento/src/app.js
@@ -1,5 +1,5 @@
-const express = require ('express')
-const routes = require ('./router')
+import express from 'express'
+import routes from './router'
 
 class App {
     constructor(){
@@ -16,4 +16,4 @@ class App {
         }
 }
 
-module.exports = new App().server;
+export default new App().server;
