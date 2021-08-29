0ec2847520a04bc80c29773fc2e6a160dd5aafce
Amauri Rodrigues dos Santos Junior <amauriaureo@gmail.com>
2021-08-28 22:31:31 -0300

    ...

---
+++ sistemaDeAgendamento/src/config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+    dialect: 'postgres',
+    host: 'localhost',
+    username: 'postgres',
+    password: 'docker',
+    databse: 'sistema',
+    define: {
+        timestamps: true,
+        underscored: true,
+        underscoredAll: true,
+        
+    }
+}
